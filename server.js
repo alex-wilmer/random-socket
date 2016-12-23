3761a6c983f73bceb632cd06520067d816299fa8
Alex Wilmer <djazium@gmail.com>
2016-12-23 20:06:52 +0000

    :sparkles: redirect url

--- server.js
+++ server.js
@@ -1,20 +1,32 @@
 import Koa from 'koa'
 import koaRouter from 'koa-router'
 
-let router = koaRouter()
+let router = koaRouter({ prefix: `/${process.env.PREFIX}` })
+
 let app = new Koa()
+
+let port = process.env.PORT || 3002
+
+let clientID = `0d2b92a89088494488cb5633a3b039f7`
+let redirectUri = `http://benevolent.ninja/${process.env.PREFIX}/redirect`
+
+let code
 
 router
   .get(`/`, (ctx, next) => {
-    ctx.body = `Hello World!`
+   ctx.body = `
+    <a href="https://api.instagram.com/oauth/authorize/?client_id=${clientID}&redirect_uri=${redirectUri}&response_type=code">Sign up</a>`
   })
-  .get(`/test`, (ctx, next) => {
-    ctx.body = `Hello World! TEST`
+  .get(`/redirect`, (ctx, next) => {
+    code = ctx.query.code
+    console.log(code) 
+    ctx.body = `Thanks! You're signed in`
   })
 
 app
   .use(router.routes())
   .use(router.allowedMethods())
 
-app.listen(3000)
-console.log(`Listening on 3000`)
+app.listen(port)
+console.log(`Listening on port: ${port}`)
+
