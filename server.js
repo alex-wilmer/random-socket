3dba8dbf5d4f22781642c3a782986921f85f2929
Alex Wilmer <alex.wilmer@oicr.on.ca>
2016-12-23 15:48:09 -0500

    get access token

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 import Koa from 'koa'
 import koaRouter from 'koa-router'
+import fetch from 'isomorphic-fetch'
+import FormData from 'form-data'
 
 let router = koaRouter({ prefix: `/${process.env.PREFIX}` })
 
@@ -10,16 +12,45 @@ let port = process.env.PORT || 3002
 let clientID = `0d2b92a89088494488cb5633a3b039f7`
 let redirectUri = `http://benevolent.ninja/${process.env.PREFIX}/redirect`
 
-let code
+let authUrl = `https://api.instagram.com/oauth/authorize/?`
++ `client_id=${clientID}&redirect_uri=${redirectUri}&response_type=code`
+
+let tokenResponse
+
+let getAccessToken = async code => {
+  let form = new FormData()
+
+  form.append(`client_id`, clientID)
+  form.append(`client_secret`, `sauce`)
+  form.append(`grant_type`, `authorization_code`)
+  form.append(`redirect_uri`, redirectUri)
+  form.append(`code`, code)
+
+  try {
+    let r = await fetch(`https://api.instagram.com/oauth/access_token`, {
+      method: `POST`,
+      body: form,
+    })
+
+    let j = await r.json()
+
+    tokenResponse = j
+
+    console.log(tokenResponse)
+  } catch(e) {
+    throw e
+  }
+}
 
 router
   .get(`/`, (ctx, next) => {
-   ctx.body = `
-    <a href="https://api.instagram.com/oauth/authorize/?client_id=${clientID}&redirect_uri=${redirectUri}&response_type=code">Sign up</a>`
+    ctx.body = `
+      <a href="${authUrl}">
+        Sign up
+      </a>`
   })
   .get(`/redirect`, (ctx, next) => {
-    code = ctx.query.code
-    console.log(code) 
+    getAccessToken(ctx.query.code)
     ctx.body = `Thanks! You're signed in`
   })
 
@@ -29,4 +60,3 @@ app
 
 app.listen(port)
 console.log(`Listening on port: ${port}`)
-
